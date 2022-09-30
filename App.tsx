7cd3679091f54a64939b57cf9e5a857376601a75
GeovaniLima <geovani.lima.silva@hotmail.com>
2022-09-30 11:07:00 -0300

    Status bar e Loading

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
-import { ActivityIndicator } from "react-native";
+//import { ActivityIndicator } from "react-native";
+import { StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Loading } from "@components/Loading";
 
 import theme from './src/theme';
 
@@ -14,8 +17,14 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      
       {
-        fontsLoaded ? <Groups /> : <ActivityIndicator />
+        fontsLoaded ? <Groups /> : <Loading />
       }
     </ThemeProvider>
   );
